fbe93d7ca686ed2052c49b1e6df06b26bcd93304
Adam Schwalm <adamschwalm@gmail.com>
2015-09-30 22:48:19 -0500

    Better setup for pypi

--- setup.py
+++ setup.py
@@ -15,17 +15,16 @@ setup(
     name='sas',
     author='Adam Schwalm',
     version='0.1',
-    license='LICENSE',
     url='https://github.com/ALSchwalm/SyntaxAwareSearch',
     download_url = 'https://github.com/ALSchwalm/SyntaxAwareSearch/tarball/0.1',
     description='A grep-like tool with syntax awareness',
     long_description=long_description,
     packages=find_packages('.', exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    install_requires=["clang", "rply"],
+    install_requires=["clang", "rply", "docopt"],
         entry_points={
         'console_scripts': [
             'sas = sas.sas:main',
         ]
     },
-    classifiers = ["grep", "syntax", "search"],
+    keywords = ["grep", "syntax", "search"],
 )

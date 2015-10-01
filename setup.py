16a909ae6f8bd4d8234562587f2e93e3001812cf
Adam Schwalm <adamschwalm@gmail.com>
2015-09-30 22:51:31 -0500

    Fixed bug in setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python
-import os
 from setuptools import setup, find_packages
-
-
-long_description = open(
-    os.path.join(
-        os.path.dirname(__file__),
-        'README.md'
-    )
-).read()
 
 
 setup(
     name='sas',
     author='Adam Schwalm',
-    version='0.1',
+    version='0.1.2',
     url='https://github.com/ALSchwalm/SyntaxAwareSearch',
-    download_url = 'https://github.com/ALSchwalm/SyntaxAwareSearch/tarball/0.1',
+    download_url='https://github.com/ALSchwalm/SyntaxAwareSearch/tarball/0.1',
     description='A grep-like tool with syntax awareness',
-    long_description=long_description,
     packages=find_packages('.', exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires=["clang", "rply", "docopt"],
         entry_points={
@@ -26,5 +16,5 @@ setup(
             'sas = sas.sas:main',
         ]
     },
-    keywords = ["grep", "syntax", "search"],
+    keywords=["grep", "syntax", "search"],
 )
